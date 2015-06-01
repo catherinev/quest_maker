abbf7a025b15bb60356efa851c8c2d0365e795d3
catherinev <catherinev@users.noreply.github.com>
2015-05-31 20:22:19 -0500

    Initialize project

---
+++ quest_maker/manage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quest_maker.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 
+                          "quest_maker.settings.settings_prod")
+
+    this_machine = os.environ.get("MACHINE", None)    
+    if this_machine == 'local':
+        os.environ["DJANGO_SETTINGS_MODULE"] = "quest_maker.settings.settings_dev"
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
