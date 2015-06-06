ad4d50fa6c828f57e0fc405ee2b8228777bd3feb
catherinev <catherinev@users.noreply.github.com>
2015-06-05 22:15:11 -0500

    update models

--- quest_maker_app/admin.py
+++ quest_maker_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Quest, Waypoint
+from .models import Quest, Waypoint, QuestTemplate, UserQuest, DailyMile
 
 
 # Register your models here.
 admin.site.register(Quest)
 admin.site.register(Waypoint)
+admin.site.register(QuestTemplate)
+admin.site.register(UserQuest)
+admin.site.register(DailyMile)
--- quest_maker_app/migrations/0001_initial.py
+++ quest_maker_app/migrations/0001_initial.py
@@ -13,12 +13,40 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='DailyMile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('miles', models.FloatField(null=True, blank=True)),
+                ('day', models.DateField()),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Quest',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=100, db_index=True)),
-                ('creator', models.ForeignKey(related_name='quests_created', to=settings.AUTH_USER_MODEL)),
-                ('users', models.ManyToManyField(related_name='quests', to=settings.AUTH_USER_MODEL)),
+                ('start_date', models.DateField(auto_now=True)),
+                ('leader', models.ForeignKey(related_name='quests_led', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='QuestTemplate',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('author', models.ForeignKey(related_name='quest_templates_created', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='UserQuest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('character', models.CharField(max_length=100)),
+                ('total_miles', models.FloatField()),
+                ('quest', models.ForeignKey(related_name='user_quests', to='quest_maker_app.Quest')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
@@ -28,7 +56,18 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=100)),
                 ('distance_from_start', models.FloatField(null=True, blank=True)),
                 ('description', models.TextField(null=True, blank=True)),
-                ('quest', models.ForeignKey(to='quest_maker_app.Quest')),
+                ('notability', models.CharField(max_length=10, choices=[(b'high', b'high'), (b'medium', b'medium'), (b'low', b'low')])),
+                ('quest_template', models.ForeignKey(to='quest_maker_app.QuestTemplate')),
             ],
+        ),
+        migrations.AddField(
+            model_name='quest',
+            name='template',
+            field=models.ForeignKey(to='quest_maker_app.QuestTemplate'),
+        ),
+        migrations.AddField(
+            model_name='quest',
+            name='users',
+            field=models.ManyToManyField(to=settings.AUTH_USER_MODEL, through='quest_maker_app.UserQuest'),
         ),
     ]
