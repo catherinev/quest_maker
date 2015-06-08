95c380b656f37bf701183ce017757dc9a1140208
xanv <xan.vong@gmail.com>
2015-06-07 19:30:22 -0500

    Add basic login page

--- quest_maker/urls.py
+++ quest_maker/urls.py
@@ -15,10 +15,21 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('quest_maker_app.urls', 
-                      namespace="quest_maker_app", 
+
+    # Authentication. See https://docs.djangoproject.com/en/1.8/topics/auth/default/
+    # By default, these views look for templates inside
+    # quest_maker/templates/registration/
+    url(r'^login/', 'django.contrib.auth.views.login', name='login'),
+
+    url(r'^password-reset/', 'django.contrib.auth.views.password_reset',
+        name='password_reset'),
+
+
+    url(r'', include('quest_maker_app.urls',
+                      namespace="quest_maker_app",
                       app_name="quest_maker_app")),
 ]
